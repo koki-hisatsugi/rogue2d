0c921ebac48705b8bd7f30996973f1755b104906
koki-hisatsugi <hisakoki.gigaleopa@gmail.com>
2022-07-06 20:03:37 +0900

    ゲーム内のターン管理順を以下のように変更 1,インプット(プレイヤーの行動を選定) 2,エネミーの行動選定 3,プレイヤーの行動(攻撃など)実行 4,移動(プレイヤーとエネミー) 5,エネミーの行動(攻撃など)実行 6,エンドフェイズ

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,13 +32,12 @@ public class GameManager : MonoBehaviour
     public enum GameState
     {
         InputStay,
+        EnemyOperateFase,
+        PlayerAct,
+        EnemyAct,
         MoveFase,
-        AttackFase,
         EndFase,
         GameEnd,
-
-        PlayerAct,
-        EnemyOperate,
     }
 
     [SerializeField] private GameState _thisGameState;
@@ -79,13 +78,14 @@ public class GameManager : MonoBehaviour
         _HPbar = GameObject.Find("HPbar").GetComponent<Slider>();
         _HPbar.value = _PlayerActorManager.GetHP;
 
-        InitGame();
+        // InitGame();
     }
 
     // async(えいしんく)をつける
     private async void Start()
     {
         _ = UpdateLoop();
+        InitGame();
     }
 
     public void InitGame()
@@ -95,6 +95,11 @@ public class GameManager : MonoBehaviour
 
     public void DungeonSet()
     {
+        // 仮、プレイヤーの向きを右向きにする
+        _PlayerActorManager.moveHorizontal = 1;
+        _PlayerActorManager.setDir(ActorDirection.RIGHT);
+
+
         boardManager.delwall();
         _a2d = boardManager.SetupScene();
         _Enemylist = boardManager.Enemylist;
@@ -168,13 +173,20 @@ public class GameManager : MonoBehaviour
             switch (_thisGameState)
             {
                 case GameState.InputStay:         // 入力待ちフェイズ
-                    await AllActionDecision();
+                    // await AllActionDecision();
+                    await PlayerAction();
                     break;
-                case GameState.AttackFase:        // 攻撃フェイズ
-                    await AllAttackExe();
+                case GameState.EnemyOperateFase:
+                    await EnemyAction();
                     break;
-                case GameState.MoveFase:          // 動きのフェイズ
-                    await AllMoveExe();
+                case GameState.PlayerAct:
+                    await PlayerActorFase();
+                    break;
+                case GameState.EnemyAct:
+                    await EnemyActorFase();
+                    break;
+                case GameState.MoveFase:
+                    await MoveFase();
                     break;
                 case GameState.EndFase:           // 終了処理のフェイズ
                     await EndProcess();
@@ -183,16 +195,13 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private async UniTask AllActionDecision()
-    {
+    private async UniTask PlayerAction(){
         bool inputTrue = false;
         await UniTask.WaitUntil(() => Input.anyKey);
         // プレイヤーの行動を選定
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
             // 矢印キーが押された場合、以下の処理を行う
-            // 入力されたことのフラグを立てる
-            inputTrue = true;
             // horizontalとverticalの値を矢印の入力に合わせる。
             int horizontal = (int)Input.GetAxisRaw("Horizontal");
             int vertical = (int)Input.GetAxisRaw("Vertical");
@@ -217,128 +226,142 @@ public class GameManager : MonoBehaviour
             if (_a2d.Get(_PlayerLocalPosX + horizontal, _PlayerLocalPosY + vertical).GetSetMapValue != BoardRemote.WallNum &&
             _a2d.Get(_PlayerLocalPosX + horizontal, _PlayerLocalPosY + vertical).GetSetMapValue != BoardRemote.EnemyNum)
             {
+                // 入力が有効だった場合のフラグを立てる
+                inputTrue = true;
                 // 障害物がない場合には、内部マップのプレイヤー位置を変える
                 _a2d.Get(_PlayerLocalPosX, _PlayerLocalPosY).GetSetMapValue = 0;
                 _a2d.Get(_PlayerLocalPosX + horizontal, _PlayerLocalPosY + vertical).GetSetMapValue = BoardRemote.PlayerNum;
+                _PlayerActorManager.ThisAction = ActorManager.ActorAction.isMove;
             }
-            else
-            {
-                return;
-            }
-            _PlayerActorManager.ThisAction = ActorManager.ActorAction.isMove;
         }
         // 攻撃ボタンを押された場合
         else if (Input.GetKeyDown(KeyCode.B))
         {
+            // 入力が有効だった場合のフラグを立てる
             inputTrue = true;
             _PlayerActorManager.ThisAction = ActorManager.ActorAction.isAttack;
         }
 
-        // エネミーの行動を選定
-        if (inputTrue)
+        if(inputTrue){
+           _thisGameState = GameState.EnemyOperateFase; // ??何に設定するか
+        }
+    }
+
+    private async UniTask EnemyAction()
+    {
+        // エネミーの行動を決定する
+        foreach (Transform enemy in _Enemys.transform)
         {
-            foreach (Transform enemy in _Enemys.transform)
+            // エネミーのスクリプトを格納しておく
+            _EnemyActorMove = enemy.gameObject.GetComponent<ActorMove>();
+            _EnemyActorManager = enemy.gameObject.GetComponent<ActorManager>();
+            // エネミーのAIをインスタンスする
+            EnemyAI EAI = new EnemyAI();
+            // エネミーの座標を格納する
+            int enemyLocalPosX = (int)enemy.gameObject.transform.position.x
+            ,enemyLocalPosY = (int)enemy.gameObject.transform.position.y;
+            CoordinateXY _CXY = EAI.AstarAlgo(enemyLocalPosX, enemyLocalPosY, _a2d);
+            if (_CXY.GetSetX == 0 && _CXY.GetSetY == 0)
             {
-                // エネミーのスクリプトを格納しておく
-                _EnemyActorMove = enemy.gameObject.GetComponent<ActorMove>();
-                _EnemyActorManager = enemy.gameObject.GetComponent<ActorManager>();
-                // エネミーのAIをインスタンスする
-                EnemyAI EAI = new EnemyAI();
-                // エネミーの座標を格納する
-                int enemyLocalPosX = (int)enemy.gameObject.transform.position.x
-                ,enemyLocalPosY = (int)enemy.gameObject.transform.position.y;
-                CoordinateXY _CXY = EAI.AstarAlgo(enemyLocalPosX, enemyLocalPosY, _a2d);
-                if (_CXY.GetSetX == 0 && _CXY.GetSetY == 0)
+                if (enemy.gameObject.transform.position.x > _CXY.GetSetPX)
                 {
-                    if (enemy.gameObject.transform.position.x > _CXY.GetSetPX)
-                    {
-                        _EnemyActorManager.setDir(ActorDirection.LEFT);
-                    }
-                    else if (enemy.gameObject.transform.position.x < _CXY.GetSetPX)
-                    {
-                        _EnemyActorManager.setDir(ActorDirection.RIGHT);
-                    }
-                    else if (enemy.gameObject.transform.position.y > _CXY.GetSetPY)
-                    {
-                        _EnemyActorManager.setDir(ActorDirection.DOWN);
-                    }
-                    else if (enemy.gameObject.transform.position.y < _CXY.GetSetPY)
-                    {
-                        _EnemyActorManager.setDir(ActorDirection.UP);
-                    }
-                    _EnemyActorManager.ThisAction = ActorManager.ActorAction.isAttack;
+                    _EnemyActorManager.setDir(ActorDirection.LEFT);
+                }
+                else if (enemy.gameObject.transform.position.x < _CXY.GetSetPX)
+                {
+                    _EnemyActorManager.setDir(ActorDirection.RIGHT);
+                }
+                else if (enemy.gameObject.transform.position.y > _CXY.GetSetPY)
+                {
+                    _EnemyActorManager.setDir(ActorDirection.DOWN);
+                }
+                else if (enemy.gameObject.transform.position.y < _CXY.GetSetPY)
+                {
+                    _EnemyActorManager.setDir(ActorDirection.UP);
+                }
+                _EnemyActorManager.ThisAction = ActorManager.ActorAction.isAttack;
+            }
+            else
+            {
+                _EnemyActorManager.ThisAction = ActorManager.ActorAction.isMove;
+                _EnemyActorManager.moveHorizontal = _CXY.GetSetX - (int)enemy.gameObject.transform.position.x;
+                _EnemyActorManager.moveVatical = _CXY.GetSetY - (int)enemy.gameObject.transform.position.y;
+                if (_a2d.Get(_CXY.GetSetX, _CXY.GetSetY).GetSetMapValue != BoardRemote.PlayerNum
+                && _a2d.Get(_CXY.GetSetX, _CXY.GetSetY).GetSetMapValue != BoardRemote.WallNum
+                && _a2d.Get(_CXY.GetSetX, _CXY.GetSetY).GetSetMapValue != BoardRemote.EnemyNum)
+                {
+                    
+                int _EnemyLocalPosX = (int)enemy.gameObject.transform.position.x
+                ,_EnemyLocalPosY = (int)enemy.gameObject.transform.position.y;
+
+                    _a2d.Get(_EnemyLocalPosX, _EnemyLocalPosY).GetSetMapValue = 0; // 自分がいなくなったマスに0をセットする
+                    _a2d.Get(_CXY.GetSetX, _CXY.GetSetY).GetSetMapValue = BoardRemote.EnemyNum; // 5をセットする
                 }
                 else
                 {
-                    _EnemyActorManager.ThisAction = ActorManager.ActorAction.isMove;
-                    _EnemyActorManager.moveHorizontal = _CXY.GetSetX - (int)enemy.gameObject.transform.position.x;
-                    _EnemyActorManager.moveVatical = _CXY.GetSetY - (int)enemy.gameObject.transform.position.y;
-                    if (_a2d.Get(_CXY.GetSetX, _CXY.GetSetY).GetSetMapValue != BoardRemote.PlayerNum
-                    && _a2d.Get(_CXY.GetSetX, _CXY.GetSetY).GetSetMapValue != BoardRemote.WallNum
-                    && _a2d.Get(_CXY.GetSetX, _CXY.GetSetY).GetSetMapValue != BoardRemote.EnemyNum)
-                    {
-                        
-                    int _EnemyLocalPosX = (int)enemy.gameObject.transform.position.x
-                    ,_EnemyLocalPosY = (int)enemy.gameObject.transform.position.y;
-
-                        _a2d.Get(_EnemyLocalPosX, _EnemyLocalPosY).GetSetMapValue = 0; // 自分がいなくなったマスに0をセットする
-                        _a2d.Get(_CXY.GetSetX, _CXY.GetSetY).GetSetMapValue = BoardRemote.EnemyNum; // 5をセットする
-                    }
-                    else
-                    {
-                        _EnemyActorManager.ThisAction = ActorManager.ActorAction.isNone;
-                    }
+                    _EnemyActorManager.ThisAction = ActorManager.ActorAction.isNone;
                 }
             }
         }
 
-        _thisGameState = GameState.MoveFase;
-        // _thisGameState = GameState.AttackFase;
+        _thisGameState = GameState.PlayerAct;
     }
 
-
-    private async UniTask AllAttackExe()
-    {
-        float playerPosX = _Player.transform.position.x, playerPosY = _Player.transform.position.y;
-        // プレイヤーの攻撃を行う
+    private async UniTask PlayerActorFase(){
+        // 攻撃が設定されている場合、
         if (_PlayerActorManager.ThisAction == ActorManager.ActorAction.isAttack)
         {
+            float playerPosX = _Player.transform.position.x, playerPosY = _Player.transform.position.y;
+            // プレイヤーの攻撃を行う
             _PlayerActorManager.Action();
             _StaminaCostTurn++; // スタミナを消費するターンを加算
+            // プレイヤーの攻撃アニメーションの終了を待つ
+            await UniTask.WaitUntil(() => _PlayerActorManager.isAttack == false);
+            // アニメーションを行った際にポジションが微妙にズレるので強制する
+            _Player.transform.position = new Vector3((float)Math.Round(playerPosX), (float)Math.Round(playerPosY), 0.0f);
+            _PlayerActorManager.ThisAction = ActorManager.ActorAction.isNone;
+
+            // エネミーの死亡判定
+            foreach (Transform enemy in _Enemys.transform)
+            {
+                _EnemyActorMove = enemy.gameObject.GetComponent<ActorMove>();
+                _EnemyActorManager = enemy.gameObject.GetComponent<ActorManager>();
+                Debug.Log(_EnemyActorManager.ThisAction);
+                if (_EnemyActorManager.GetHP <= 0)
+                {
+                    _a2d.Get((int)enemy.gameObject.transform.position.x, (int)enemy.gameObject.transform.position.y).GetSetMapValue = 0;
+                    GameObject.Find("Log").GetComponent<OutPutLog>().OutputLog(_EnemyActorManager.GetName + "は倒れた");
+                    Destroy(enemy.gameObject);
+                }
+            }
+            // エネミーがdestroyされていたらリストから削除する
+            _Enemylist.Remove(null);
         }
-        // プレイヤーの攻撃アニメーションの終了を待つ
-        await UniTask.WaitUntil(() => _PlayerActorManager.isAttack == false);
-        // アニメーションを行った際にポジションが微妙にズレるので強制する
-        _Player.transform.position = new Vector3((float)Math.Round(playerPosX), (float)Math.Round(playerPosY), 0.0f);
+        // 移動行動フェイズに移行する
+        _thisGameState = GameState.MoveFase;
+    }
+
+    private async UniTask EnemyActorFase(){
         // エネミーの攻撃を行う
         foreach (Transform enemy in _Enemys.transform)
         {
-            float EnemyPosX = enemy.transform.position.x, EnemyPosY = enemy.transform.position.y;
             _EnemyActorMove = enemy.gameObject.GetComponent<ActorMove>();
             _EnemyActorManager = enemy.gameObject.GetComponent<ActorManager>();
-            if (_EnemyActorManager.ThisAction == ActorManager.ActorAction.isAttack)
-            {
+            if (_EnemyActorManager.ThisAction == ActorManager.ActorAction.isAttack){
+                float EnemyPosX = enemy.transform.position.x, EnemyPosY = enemy.transform.position.y;
                 _EnemyActorManager.Action();
+                // エネミーの攻撃アニメーションの終了を待つ
+                await UniTask.WaitUntil(() => _EnemyActorManager.isAttack == false);
+                // アニメーションを行った際にポジションが微妙にズレるので強制する
+                enemy.transform.position = new Vector3((float)Math.Round(EnemyPosX), (float)Math.Round(EnemyPosY), 0.0f);
             }
-            // エネミーの攻撃アニメーションの終了を待つ
-            await UniTask.WaitUntil(() => _EnemyActorManager.isAttack == false);
-            // アニメーションを行った際にポジションが微妙にズレるので強制する
-            enemy.transform.position = new Vector3((float)Math.Round(EnemyPosX), (float)Math.Round(EnemyPosY), 0.0f);
         }
-        // プレイヤーのアクションをNoneにする
-        _PlayerActorManager.ThisAction = ActorManager.ActorAction.isNone;
-        // 全てのエネミーのアクションをNoneにする
-        foreach (Transform enemy in _Enemys.transform)
-        {
-            _EnemyActorMove = enemy.gameObject.GetComponent<ActorMove>();
-            _EnemyActorManager = enemy.gameObject.GetComponent<ActorManager>();
-            _EnemyActorManager.ThisAction = ActorManager.ActorAction.isNone;
-        }
+
+        // エンドフェイズに移行する
         _thisGameState = GameState.EndFase;
-        // _thisGameState = GameState.MoveFase;
     }
-    private async UniTask AllMoveExe()
-    {
+
+    private async UniTask MoveFase(){
         // プレイヤーの移動を行う
         if (_PlayerActorManager.ThisAction == ActorManager.ActorAction.isMove)
         {
@@ -361,31 +384,17 @@ public class GameManager : MonoBehaviour
         }
         // プレイヤーとエネミーが移動し終わるのを待つ
         await UniTask.WaitUntil(() => _PlayerActorMove.isMoveing == false && _EnemyActorMove.isMoveing == false);
-        //AutoMapping();
-        _thisGameState = GameState.AttackFase;
-        // _thisGameState = GameState.EndFase;
+        // エネミーの行動フェイズに移行する
+        _thisGameState = GameState.EnemyAct;
     }
 
     private async UniTask EndProcess()
     {
-        // エネミーの死亡判定
-        foreach (Transform enemy in _Enemys.transform)
-        {
-            _EnemyActorMove = enemy.gameObject.GetComponent<ActorMove>();
-            _EnemyActorManager = enemy.gameObject.GetComponent<ActorManager>();
-            Debug.Log(_EnemyActorManager.ThisAction);
-            if (_EnemyActorManager.GetHP <= 0)
-            {
-                _a2d.Get((int)enemy.gameObject.transform.position.x, (int)enemy.gameObject.transform.position.y).GetSetMapValue = 0;
-                GameObject.Find("Log").GetComponent<OutPutLog>().OutputLog(_EnemyActorManager.GetName + "は倒れた");
-                Destroy(enemy.gameObject);
-            }
-        }
         _Enemylist.Remove(null);
         // プレイヤーの攻撃アニメーションの終了を待つ
         await UniTask.WaitUntil(() => _PlayerActorManager.isAttack == false);
         // スタミナ消費を判定し、消費するターンならスタミナを消費
-        if (_StaminaCostTurn > 3){
+        if (_StaminaCostTurn > 5){
             _PlayerActorManager.StaminaCost();
             _StaminaCostTurn = 0;
         }
@@ -396,6 +405,7 @@ public class GameManager : MonoBehaviour
             if(_Player.GetComponent<ItemGetter>().isOnExit){
                 _Player.GetComponent<ItemGetter>().isOnExit = false;
                 DungeonSet();
+                AutoMapping();
             }
         }
         _thisGameState = GameState.InputStay;
